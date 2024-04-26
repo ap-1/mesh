566197ee3fb8afa1b3e95287ee314d912a21232b
Anish Pallati <anishp@duck.com>
2024-04-26 13:04:23 -0400

    style: Remove unused useEffectEvent import

--- src/hooks/use-paste-image.ts
+++ src/hooks/use-paste-image.ts
@@ -1,29 +1,29 @@
-import { experimental_useEffectEvent, useEffect } from "react";
+import { useEffect } from "react";
 
 export function usePasteImage(
-	cb: (image: File) => any,
-	options: AddEventListenerOptions = {}
+  cb: (image: File) => any,
+  options: AddEventListenerOptions = {}
 ) {
-	useEffect(() => {
-		const handler: EventListenerOrEventListenerObject = (e) => {
-			const event = e as ClipboardEvent;
-			const items = event.clipboardData?.items || [];
+  useEffect(() => {
+    const handler: EventListenerOrEventListenerObject = (e) => {
+      const event = e as ClipboardEvent;
+      const items = event.clipboardData?.items || [];
 
-			for (let i = 0; i < items.length; i++) {
-				const item = items[i];
-				if (item.kind === "file" && item.type.startsWith("image/")) {
-					const image = item.getAsFile();
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        if (item.kind === "file" && item.type.startsWith("image/")) {
+          const image = item.getAsFile();
 
-					if (image) {
-						cb(image);
-						break;
-					}
-				}
-			}
-		};
+          if (image) {
+            cb(image);
+            break;
+          }
+        }
+      }
+    };
 
-		window.addEventListener("paste", handler, options);
+    window.addEventListener("paste", handler, options);
 
-		return () => window.removeEventListener("paste", handler, options);
-	}, [options, cb]);
+    return () => window.removeEventListener("paste", handler, options);
+  }, [options, cb]);
 }
