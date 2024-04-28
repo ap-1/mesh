b8af8d8848b6f9cdda3f3cbd6f361eae1c635d37
Anish Pallati <anishp@duck.com>
2024-04-27 21:10:10 -0400

    style: Adjust type import

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -2,48 +2,48 @@ import { cn } from "@/lib/utils";
 
 import { ArrowUpRight } from "lucide-react";
 import {
-	type LinkProps as NextLinkProps,
-	default as NextLink,
+  default as NextLink,
+  type LinkProps as NextLinkProps,
 } from "next/link";
-import { type PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
 
 type BaseLinkProps = Omit<
-	React.AnchorHTMLAttributes<HTMLAnchorElement>,
-	keyof NextLinkProps
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  keyof NextLinkProps
 > &
-	NextLinkProps &
-	React.RefAttributes<HTMLAnchorElement>;
+  NextLinkProps &
+  React.RefAttributes<HTMLAnchorElement>;
 
 interface LinkProps extends Omit<BaseLinkProps, "target"> {
-	text: string;
-	unstyled?: boolean;
-	external?: boolean;
+  text: string;
+  unstyled?: boolean;
+  external?: boolean;
 }
 
 export const Link = ({
-	text,
-	unstyled,
-	external,
-	className,
-	...rest
+  text,
+  unstyled,
+  external,
+  className,
+  ...rest
 }: PropsWithChildren<LinkProps>) => {
-	return (
-		<NextLink
-			passHref
-			legacyBehavior
-			target={external ? "_blank" : "_self"}
-			{...rest}
-		>
-			<a
-				className={cn(
-					unstyled || "text-cyan-500 hover:underline transition-all",
-					"inline-block",
-					className
-				)}
-			>
-				{text}
-				<ArrowUpRight className="text-cyan-500 inline-block w-2 h-2 mb-4 text-muted-foreground" />
-			</a>
-		</NextLink>
-	);
+  return (
+    <NextLink
+      passHref
+      legacyBehavior
+      target={external ? "_blank" : "_self"}
+      {...rest}
+    >
+      <a
+        className={cn(
+          unstyled || "text-cyan-500 hover:underline transition-all",
+          "inline-block",
+          className,
+        )}
+      >
+        {text}
+        <ArrowUpRight className="text-cyan-500 inline-block w-2 h-2 mb-4 text-muted-foreground" />
+      </a>
+    </NextLink>
+  );
 };
