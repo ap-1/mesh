d430ac7d6c84fd9670db77e47b6853b08d62736b
Anish Pallati <anishp@duck.com>
2024-04-30 23:53:42 -0400

    style: Switch from spaces to tab indentation

--- src/auth/validate-request.ts
+++ src/auth/validate-request.ts
@@ -6,40 +6,40 @@ import { lucia } from "@/auth";
 import type { Session, User } from "lucia";
 
 export const validateRequest = cache(
-  async (): Promise<
-    { user: User; session: Session } | { user: null; session: null }
-  > => {
-    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
-    if (!sessionId) {
-      return {
-        user: null,
-        session: null,
-      };
-    }
+	async (): Promise<
+		{ user: User; session: Session } | { user: null; session: null }
+	> => {
+		const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+		if (!sessionId) {
+			return {
+				user: null,
+				session: null,
+			};
+		}
 
-    const result = await lucia.validateSession(sessionId);
+		const result = await lucia.validateSession(sessionId);
 
-    // Cannot set cookie when rendering page
-    try {
-      if (result.session?.fresh) {
-        const sessionCookie = lucia.createSessionCookie(
-          result.session.id
-        );
-        cookies().set(
-          sessionCookie.name,
-          sessionCookie.value,
-          sessionCookie.attributes
-        );
-      }
-      if (!result.session) {
-        const sessionCookie = lucia.createBlankSessionCookie();
-        cookies().set(
-          sessionCookie.name,
-          sessionCookie.value,
-          sessionCookie.attributes
-        );
-      }
-    } catch { }
-    return result;
-  }
+		// Cannot set cookie when rendering page
+		try {
+			if (result.session?.fresh) {
+				const sessionCookie = lucia.createSessionCookie(
+					result.session.id
+				);
+				cookies().set(
+					sessionCookie.name,
+					sessionCookie.value,
+					sessionCookie.attributes
+				);
+			}
+			if (!result.session) {
+				const sessionCookie = lucia.createBlankSessionCookie();
+				cookies().set(
+					sessionCookie.name,
+					sessionCookie.value,
+					sessionCookie.attributes
+				);
+			}
+		} catch { }
+		return result;
+	}
 );
